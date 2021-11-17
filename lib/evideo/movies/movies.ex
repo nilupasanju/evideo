469d8060b228f68aa558b659d91d8b6f4386474d
Nilupa Dhanawala <nilupa.sanju@gmail.com>
2021-11-17 23:24:51 +1100

    create address tables

--- lib/evideo/movies/movies.ex
+++ lib/evideo/movies/movies.ex
@@ -31,4 +31,36 @@ defmodule Evideo.Movies do
   def delete_movie(id) do
     Repo.get!(Movie, id) |> Repo.delete!
   end
+  #genre
+  def create_genre(attrs) do
+    %Movie{}
+    |> Movie.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_genre(id) do
+    Repo.get(Movie, id)
+  end
+
+  def update_genre(id, attrs) do
+    case get_movie(id) do
+      nil ->
+        {:error, :not_found}
+
+      movie ->
+        movie
+        |> Movie.changeset(attrs)
+        |> Repo.update()
+    end
+  end
+
+  def list_genre() do
+    Repo.all(Movie)
+  end
+
+  def delete_genre(id) do
+    Repo.get!(Movie, id) |> Repo.delete!
+  end
+
+
 end
