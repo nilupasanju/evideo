e112265aecab7ba8ac7460f3c730e309abe94bca
nilu <nilupa.sanju@gmail.com>
2022-05-28 23:56:04 +1000

    add migration for create staff and necessary functions

---
+++ lib/evideo/staffs/staff.ex
@@ -0,0 +1,22 @@
+defmodule Evideo.Staffs.Staff do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Evideo.Staffs.LoginStaff
+
+  schema "staff" do
+    field :login_id, :integer
+    field :phone, :string, size: 50
+    field :email, :string, size: 50
+    field :gender, :string, size: 8
+    field :wage, :integer
+
+    belongs_to :login_staff, LoginStaff, define_field: false
+    timestamps()
+  end
+
+  @doc false
+  def changeset(staff, attrs) do
+   staff
+    |> cast(attrs, [:login_id, :phone, :email, :gender, :wage])
+  end
+end
